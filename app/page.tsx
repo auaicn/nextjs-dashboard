363ddeb97b1165c58a30a50269b71273254ffded
Kyung Ho, Park <crorowslowpp@gmail.com>
2025-03-15 11:43:05 +0900

    binding / url param segment / error handling (general, 404)

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "@/app/ui/home.module.css";
 import clsx from "clsx";
 import { lusitana } from "./ui/fonts";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
       <div
         className={clsx(
           "flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52",
